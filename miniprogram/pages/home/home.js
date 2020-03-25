3e62a867f1f952818a53bb4efc8e045cff806bca
王龙 <wanglong8018@qq.com>
2020-03-25 23:37:25 +0800

    stage1

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -3,24 +3,47 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    nickName: '请点击左侧头像获取授权信息.',
-    logged: false,
+    avatarUrl: './user-unlogin.jpg',
+    metoo: '请点击上方头像获取授权信息.',
+    nickName: '',
+    welcome: '',
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
-  onLoad: function (options) {
-    var that = this;
-    /**
-     * 获取用户信息
-     */
-    wx.getUserInfo({
-      success: function (res) {
-        console.log(res);
-        that.setData({
+  onLoad: function () {
+    if (app.globalData.hasUserInfo) {
+      this.setData({
+        metoo: 'Hello!',
+        avatarUrl: app.globalData.userInfo.avatarUrl,
+        nickName: app.globalData.userInfo.nickName,
+        welcome: '欢迎使用e游趣玩公众平台!',
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          metoo: 'Hello!',
           avatarUrl: res.userInfo.avatarUrl,
-          nickName: '"' + res.userInfo.nickName + '"' + '欢迎使用e游平台!',
+          nickName: res.userInfo.nickName,
+          welcome: '欢迎使用e游趣玩公众平台!',
+          hasUserInfo: true
         })
       }
-    })
+    }
   },
+
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      metoo: 'Hello!',
+      avatarUrl: e.detail.userInfo.avatarUrl,
+      nickName: e.detail.userInfo.nickName,
+      welcome: '欢迎使用e游趣玩公众平台!',
+      hasUserInfo: true
+    })
+  }
 })
