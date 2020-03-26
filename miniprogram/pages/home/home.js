6cc917bc38d70f3dedb9ea2599f6952c77ed02cf
王龙 <wanglong8018@qq.com>
2020-03-26 16:47:46 +0800

    stage1

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,14 +1,15 @@
 //home.js
 const app = getApp()
+const db = wx.cloud.database()
 const Img2Base64 = require('../../constants/img2Base64.js');
 var idinfolist = [
-  { code: "用户", text: '123' },
-  { code: "用户等级", text: '0' },
-  { code: "累计消费", text: '0' },
-  { code: "距离下一等级", text: '0' },
-  { code: "注册日期", text: '0' },
-  { code: "最后消费日期", text: '0' },
-  { code: "最后登录日期", text: '0' }
+  { code: "用户", text: '0.00' },
+  { code: "用户等级", text: '普通用户' },
+  { code: "累计消费", text: '0.00' },
+  { code: "距离下一等级", text: '30.00' },
+  { code: "注册日期", text: '2020.03.26' },
+  { code: "最后消费日期", text: '-' },
+  { code: "最后登录日期", text: '2020.03.26' }
 ]
 
 Page({
@@ -20,8 +21,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     listData: idinfolist,
-    inputValue: '', //用于显示输入语句
-    searchinput: '' //用户输入的查询语句
+    openid: ''
   },
 
   onLoad: function () {
@@ -45,6 +45,78 @@ Page({
         }
       }
     })
+
+    // 调用云函数,获取openid
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] 调用成功 openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+
+    /******* 用户注册 *******/
+    db.collection('users').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        console.log('[数据库] [查询记录] 成功: ', res)
+        var lastLoadDate = 'listData[6]';
+        var registerDate = 'listData[4]';
+        this.setData({
+          [lastLoadDate]: { code: "最后登陆日期", text: res.data[0].lastLoadDate },
+          [registerDate]: { code: "注册日期", text: res.data[0].registerDate },
+        })
+        var TIME = new Date();
+        const year = TIME.getFullYear().toString();
+        const month = (TIME.getMonth() + 1 < 10) ? '0' + (TIME.getMonth() + 1).toString() : (TIME.getMonth() + 1).toString();
+        const day = (TIME.getDate() < 10) ? '0' + TIME.getDate().toString() : TIME.getDate().toString();
+        const hour = (TIME.getHours() < 10) ? '0' + TIME.getHours().toString() : TIME.getHours().toString();
+        const munite = (TIME.getMinutes() < 10) ? '0' + TIME.getMinutes().toString() : TIME.getMinutes().toString();
+        const second = (TIME.getSeconds() < 10) ? '0' + TIME.getSeconds().toString() : TIME.getSeconds().toString();
+        const formatDate = [year, month, day].join('.') + ' ' + [hour, munite, second].join(':');
+        if(res.data.length!=0){
+          console.log('该用户已注册,更新数据库记录.')
+          db.collection('users').doc(res.data[0]._id).update({
+            data: {
+              lastLoadDate: formatDate.toString()
+            },
+            success: res => {
+              console.log('[数据库] [更新记录] 成功.')
+            },
+            fail: err => {
+              console.error('[数据库] [更新记录] 失败：', err)
+            }
+          })
+        }
+        else{
+          console.log('新用户，新增数据库记录.')
+          db.collection('users').add({
+            data: {
+              level: 0,
+              totalPay: 0.00,
+              nextLevel: 30.00,
+              registerDate: formatDate.toString(),
+              lastPayDate: '-',
+              lastLoadDate: formatDate.toString()
+            },
+            success: res => {
+              console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+            },
+            fail: err => {
+              console.error('[数据库] [新增记录] 失败：', err)
+            }
+          })
+        }
+      },
+      fail: err => {
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
   },
 
   onGetUserInfo: function (e) {
@@ -59,5 +131,6 @@ Page({
         hasUserInfo: true
       })
     }
-  }
+  },
+
 })
